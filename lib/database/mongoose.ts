61585b6b74454ebb4d9366fc77b4c933621f3501
Natasha <145199476+ntshap@users.noreply.github.com>
2024-12-26 21:34:04 +0700

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,44 +1,48 @@
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
+ conn: Mongoose | null;
+ promise: Promise<Mongoose> | null;
 }
 
 declare global {
-  var mongoose: MongooseConnection | undefined;
+ var mongoose: MongooseConnection | undefined;
 }
 
 const cached: MongooseConnection = global.mongoose || {
-  conn: null,
-  promise: null
+ conn: null,
+ promise: null
 };
 
 if (!global.mongoose) {
-  global.mongoose = cached;
+ global.mongoose = cached;
 }
 
 export const connectToDatabase = async (): Promise<Mongoose> => {
-  if (cached.conn) return cached.conn;
+ if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) {
-    throw new Error('Missing MONGODB_URL environment variable');
-  }
+ if (!MONGODB_URI) {
+   throw new Error('Missing MONGODB_URI environment variable');
+ }
 
-  cached.promise = cached.promise || 
-    mongoose.connect(MONGODB_URL, {
-      dbName: 'imaginify',
-      bufferCommands: false
-    });
+ cached.promise = cached.promise || 
+   mongoose.connect(MONGODB_URI, {
+     dbName: 'imaginify',
+     bufferCommands: false,
+     retryWrites: false,
+     tls: true,
+     authMechanism: 'SCRAM-SHA-256',
+     maxIdleTimeMS: 120000
+   });
 
-  try {
-    cached.conn = await cached.promise;
-  } catch (error) {
-    cached.promise = null;
-    throw error;
-  }
+ try {
+   cached.conn = await cached.promise;
+ } catch (error) {
+   cached.promise = null;
+   throw error;
+ }
 
-  return cached.conn;
+ return cached.conn;
 }
